aa2ff25b6edab29183ef59798fe5e546f5677aa7
Anand Satheesh <anands282@gmail.com>
2024-06-06 18:46:03 +0530

    loops added

---
+++ forloop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+COLOURS="red green blue"
+for COLOR in $COLOURS
+do
+	echo " This is the color in use $COLOR"
+done
--- tests.sh
+++ tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-MY_SHELL="bash1"
+MY_SHELL="csh"
 
 if [ "$MY_SHELL" = "bash" ]
 then
 	echo " You seem to be using the bash shell"
+elif [[ "$MY_SHELL" = "csh" ]]; then
+	echo " You are using a csh shell"
 else
 	echo "You seem to be on a different shell"
 fi
