b307d4e8c921f41487dd6681815b4e4b4a98ae67
Anand Satheesh <anands282@gmail.com>
2024-06-06 19:05:14 +0530

    updated sample script

--- sample_script.sh
+++ sample_script.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# The commands in a script will be executed using the current shell of the user executing that script
+# if a shebang followed by a path to an interpreter are not specified on the first line of the script.
 echo "Scripting is fun"
