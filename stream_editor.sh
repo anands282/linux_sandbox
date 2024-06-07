0a07f6725fd7eb1d57d0701cc17e5d6f9bbdb5c5
Anand Satheesh <anands282@gmail.com>
2024-06-07 09:29:58 +0530

    stream editor

--- stream_editor.sh
+++ stream_editor.sh
@@ -2,6 +2,8 @@
 
 # sed stands for stream editor
 # sed commands are used to do basic text transformations on streams
+# sed matching is case sensitive
+# sed 's/search-pattern/replacement-string/flags'
 
 echo 'Dwight is the assistant regional manager.' > manager.txt
 cat manager.txt
