4a499d2eb9b86ad5ca7d104568e006b322d2aaee
Anand Satheesh <anands282@gmail.com>
2024-06-07 08:40:54 +0530

    updated

---
+++ case_statement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+case "$1" in
+	"Anand")
+		echo "You have entered Anand"
+		;;
+   "Anamika")
+		echo "You have entered Anamika"
+		;;
+		*)
+		echo "you have entered something else"
+		;;
+esac
+
+
+	
