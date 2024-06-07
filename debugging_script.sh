2c651770315b2fc6d693561f13131328fb558d19
Anand Satheesh <anands282@gmail.com>
2024-06-07 08:58:21 +0530

    updated debugging

---
+++ debugging_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+# -x = prints commands as they execute
+# this is called print debugging set -x to start debugging, set +x to stop debugging
+TEST_VAR="test"
+echo $TEST_VAR
+
+
+# set -x
+# TEST_VAR="test"
+# echo $TEST_VAR
+# set +x
