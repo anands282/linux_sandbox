68e5a6161cf4745add576c1b3b9a49dfc695bf4d
Anand Satheesh <anands282@gmail.com>
2024-06-07 09:08:20 +0530

    debugging code added

---
+++ debugging2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+# -e Exit on error can be combined with x also
+
+
+FILE_NAME = "no_such_file"
+ls $FILE_NAME
+echo $FILE_NAME
---
+++ debugging3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -vx
+# prints shell input lines as they are read
+
+TEST_VAR="test"
+echo $TEST_VAR
