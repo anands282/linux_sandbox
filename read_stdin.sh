f21f26ef232c8d5ae54d90d6d3715bee1add4516
Anand Satheesh <anands282@gmail.com>
2024-06-07 09:31:57 +0530

    updates

--- read_stdin.sh
+++ read_stdin.sh
@@ -3,3 +3,8 @@
 read -p "Please enter the parameters to read: " PARAM1 PARAM2
 echo "this is param 1 $PARAM1"
 echo "this is param 2 $PARAM2"
+
+# ./read_stdin.sh
+# Please enter the parameters to read: Anand Satheesh is my name
+# this is param 1 Anand
+# this is param 2 Satheesh is my name
--- variables.sh
+++ variables.sh
@@ -3,10 +3,10 @@ MY_NAME="Anand Satheesh"
 echo "My name is $MY_NAME and I live in India"
 echo "My name is ##${MY_NAME}## and I live in India"
 
-#Assigning the output of a command to a variable
+#Assigning the output of a command to a variable using ()
 
 SERVER_NAME=$(hostname)
 echo "You are running this script on ${SERVER_NAME}"
 
-SERVER=$(echo "Hello")
+SERVER=$(echo "aws")
 echo "You are running this script on ${SERVER}"
